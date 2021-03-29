bab69d7496cafe3eec5fb8c39966629a612d5610
Rudi B. Svartveit <rudsvar@gmail.com>
2021-03-29 12:57:19 +0200

    Add WIP random component to hotdrink-ts

--- hotdrink-comparison/hotdrink-typescript/index.js
+++ hotdrink-comparison/hotdrink-typescript/index.js
@@ -101,6 +101,118 @@ function make_ladder(n_variables) {
     return model.component();
 }
 
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function swap_remove(array, index) {
+    if (index === array.length - 1) {
+        return array.pop();
+    }
+    let last = array.pop();
+    let to_remove = array[index];
+    array[index] = last;
+    return to_remove;
+}
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+function make_random(n_constraints) {
+    var model = new hd.ComponentBuilder();
+    let current_constraints = 0;
+    let variables_per_constraint = 5;
+
+    // Start with n_constraints + 1, as each constraint should use at least 1 unused variable
+    let n_variables = n_constraints + 1;
+    let variables = [];
+    for (let i = 0; i < n_variables; i++) {
+        variables.push(i);
+        model.variable(vname(i), 0);
+    }
+
+    // Add all current variables
+    let used = [0];
+    let unused = variables.slice();
+    while (current_constraints < n_constraints) {
+        // If there are no more unused variables, add a new one
+        if (unused.length == 0) {
+            variables.push(n_variables);
+            unused.push(n_variables);
+            model.variable(vname(n_variables), 0);
+            n_variables++;
+        }
+
+        // Pick a random used variable
+        let used_idx = random(0, used.length);
+        let used_val = used[used_idx];
+
+        // Pick a random unused variable
+        let unused_idx = random(0, unused.length);
+        let unused_val = swap_remove(unused, unused_idx);
+        // Add it to used
+        used.push(unused_val);
+
+        // Add remaining variables, can be used or unused
+        let buffer = [];
+        for (let i = 0; i < variables_per_constraint - 2; i++) {
+            let idx = random(0, variables.length);
+            // Remove temporarily to avoid duplicates
+            let val = swap_remove(variables, idx);
+            buffer.push(val);
+        }
+        // Add back to variables
+        variables.push(...buffer);
+        buffer = buffer.filter(x => x != used_val && x != unused_val);
+
+        // console.log("Buffer", buffer);
+        // console.log("Used", used_val);
+        // console.log("Unused", unused_val);
+        // Add all variables to constraint
+        let inputs_string = "";
+        for (let v of buffer) {
+            inputs_string += vname(v) + ", ";
+        }
+        model.constraint(inputs_string + vname(used_val) + ", " + vname(unused_val));
+
+        // Add methods that write to used and unused
+        model.method(inputs_string + vname(unused_val) + " -> " + vname(used_val), arr => 0);
+        model.method(inputs_string + vname(used_val) + " -> " + vname(unused_val), arr => 0);
+
+        // Add remaining methods
+        let n_methods = random(0, 3);
+        // console.log("Adding", n_methods, " methods");
+        // console.log(buffer);
+        for (let i = 0; i < Math.min(n_methods, buffer.length); i++) {
+            let output = buffer[i];
+            // console.log("Output", output);
+            let inputs = [];
+            for (let j = 0; j < buffer.length; j++) {
+                // Ignore the output
+                if (i != j) {
+                    inputs.push(buffer[j]);
+                }
+            }
+            // console.log("Inputs", inputs);
+            let inputs_string = "";
+            for (let j = 0; j < inputs.length; j++) {
+                inputs_string += vname(inputs[j]) + ", ";
+            }
+            let mstr = inputs_string + vname(used_val) + ", " + vname(unused_val) + " -> " + vname(output);
+            // console.log("mstr:", mstr);
+            model.method(mstr, arr => 0);
+        }
+
+        current_constraints++;
+    }
+
+    return model;
+}
+
 function bench_component(name, make_component, n_variables) {
     var pm = new hd.PropertyModel();
     pm.add(make_component(n_variables));
@@ -123,8 +235,9 @@ function bench_component_update_variable(name, make_component, n_variables) {
     let total = 0;
     let n_samples = 5;
     for (let i = 0; i < n_samples; i++) {
-        let random_number = Math.floor(Math.random() * n_variables);
-        for (let [k, v] of Object.entries(pm.variables)) {
+        let entries = Object.entries(pm.variables);
+        let random_number = Math.floor(Math.random() * entries.length);
+        for (let [k, v] of entries) {
             if (k.startsWith(vname(random_number))) {
                 pm.variableChanged(v);
             }
@@ -138,7 +251,7 @@ function bench_component_update_variable(name, make_component, n_variables) {
 }
 
 function bench_components(entries) {
-    for (let n_variables of [5000]) {
+    for (let n_variables of [100]) {
         for (let entry of entries) {
             let name = entry.name;
             let make_component = entry.make_component;
@@ -149,10 +262,11 @@ function bench_components(entries) {
 }
 
 bench_components([
-    { name: "empty", make_component: make_empty },
-    { name: "dense", make_component: make_dense },
+    // { name: "empty", make_component: make_empty },
+    // { name: "dense", make_component: make_dense },
     { name: "linear-oneway", make_component: make_linear_oneway },
     { name: "linear-twoway", make_component: make_linear_twoway },
     { name: "ladder", make_component: make_ladder },
     { name: "unprunable", make_component: make_unprunable },
+    { name: "random", make_component: x => make_random(x).component() },
 ]);
