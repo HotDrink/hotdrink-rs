5c3ca17da2312f73c37ab03b4fc67d7610d3e302
Rudi B. Svartveit <rudsvar@gmail.com>
2021-04-01 16:40:14 +0200

    Switch to random component in hierarchical planner example

--- hotdrink-rs/examples/hierarchical_planner.rs
+++ hotdrink-rs/examples/hierarchical_planner.rs
@@ -1,13 +1,11 @@
 use hotdrink_rs::{
-    algorithms::hierarchical_planner::hierarchical_planner, data::traits::ComponentSpec,
-    examples::constraint_systems::make_dense_cs,
+    algorithms::hierarchical_planner::hierarchical_planner,
+    examples::components::{random::Random, ComponentFactory},
 };
 
 fn main() {
     env_logger::init();
-    let cs = make_dense_cs::<()>(1, 100000);
-    let comp = cs.component("0").unwrap();
-    let ranking: Vec<_> = (0..comp.n_variables()).collect();
-    let plan = hierarchical_planner(comp, &ranking);
+    let comp = Random::build::<()>(10000);
+    let plan = hierarchical_planner(&comp, &comp.ranking());
     dbg!(plan.is_ok());
 }
