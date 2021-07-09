325185f47ffc5b39199d62bb83a297b309ec0cc8
Rudi B. Svartveit <rudsvar@gmail.com>
2021-07-09 12:51:58 +0200

    Rename run_update to run_solve

--- hotdrink-rs/examples/run_solve.rs
+++ hotdrink-rs/examples/run_solve.rs
@@ -2,7 +2,7 @@ use hotdrink_rs::examples::components::{ComponentFactory, Random};
 
 fn main() {
     env_logger::init();
-    let mut component = Random::build::<()>(100000);
+    let mut component = Random::build::<()>(10000);
     let result = component.solve();
     println!("{:?}", result);
 }
