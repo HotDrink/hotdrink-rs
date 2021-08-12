8bb29ce484128a7a777d18297d296d45708357c4
Rudi B. Svartveit <rudsvar@gmail.com>
2021-08-12 14:23:54 +0200

    Add default text to text search example

--- examples/hotdrink-wasm-multi-threaded/src/examples/text_search.rs
+++ examples/hotdrink-wasm-multi-threaded/src/examples/text_search.rs
@@ -5,10 +5,16 @@ use wasm_bindgen::prelude::wasm_bindgen;
 pub fn text_search_inner() -> ConstraintSystem<StringValue> {
     let a = component! {
         component Component {
-            let input: String, query: String, output: String;
+            let input: String = "\
+At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.
+Et harum quidem rerum facilis est et expedita distinctio.
+Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.
+Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae.
+Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.
+", query: String, output: String;
             constraint Filter {
                 search(input: &String, query: &String) -> [output] = {
-                    slow_fib(40);
+                    slow_fib(41);
                     let output: String = input.lines()
                         .filter(|line| line.contains(query))
                         .collect::<Vec<&str>>()
